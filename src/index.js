395ad519f83aa2a974bed7d07d52037a3b673dd0
AndriyP0 <andreopopovich@gmail.com>
2025-12-14 22:51:55 +0200

    apdate index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 "use strict";
 
-for (let i = 2; i <= 100; i++) {
-  let isSimple = true;
+outer: for (let i = 2; i <= 100; i++) {
 
   for (let j = 2; j < i; j++) {
     if (i % j === 0) {
-      isSimple = false;
-      break;
+      continue outer; 
     }
   }
 
-  if (isSimple) {
-    console.log(i);
-  }
+  console.log(i);
 }
